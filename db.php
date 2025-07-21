3daa4d6552090e9b7198a7c4be14bcf419cfd33a
GiselleBalieiro <gisellefbalieiro@outlook.com>
2025-07-21 09:58:39 -0300

    fix: ajusta arquivo

--- db.php
+++ db.php
@@ -1,14 +1,14 @@
 <?php
 
 
-// Carrega variáveis de ambiente, se estiver em ambiente local com .env
-if (file_exists(__DIR__ . '/vendor/autoload.php')) {
-    require __DIR__ . '/vendor/autoload.php';
-    if (class_exists('Dotenv\Dotenv')) {
-        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-        $dotenv->load();
-    }
-}
+// // Carrega variáveis de ambiente, se estiver em ambiente local com .env
+// if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+//     require __DIR__ . '/vendor/autoload.php';
+//     if (class_exists('Dotenv\Dotenv')) {
+//         $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+//         $dotenv->load();
+//     }
+// }
 
 // Alternativa para Railway, Heroku ou servidores que usam env diretamente
 $host = $_ENV["DATABASE_HOST"] ?? getenv('DATABASE_HOST');
