e6018e758c6b7b10ea1392180799c03d883bb429
GiselleBalieiro <gisellefbalieiro@outlook.com>
2025-08-03 14:17:19 -0300

    fix: ajusta arquivo de conexão

--- db.php
+++ db.php
@@ -1,31 +1,29 @@
 <?php
 
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+    require __DIR__ . '/vendor/autoload.php';
+    if (class_exists('Dotenv\Dotenv')) {
+        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+        $dotenv->load();
+    }
+}
 
-// // Carrega variáveis de ambiente, se estiver em ambiente local com .env
-// if (file_exists(__DIR__ . '/vendor/autoload.php')) {
-//     require __DIR__ . '/vendor/autoload.php';
-//     if (class_exists('Dotenv\Dotenv')) {
-//         $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-//         $dotenv->load();
-//     }
-// }
-
-// Alternativa para Railway, Heroku ou servidores que usam env diretamente
 $host = $_ENV["DATABASE_HOST"] ?? getenv('DATABASE_HOST');
 $dbname = $_ENV["DATABASE"] ?? getenv('DATABASE');
 $username = $_ENV["DATABASE_USERNAME"] ?? getenv('DATABASE_USERNAME');
 $password = $_ENV["DATABASE_PASSWORD"] ?? getenv('DATABASE_PASSWORD');
-
-// var_dump($host, $dbname, $username, $password);
-// exit;
+$port = $_ENV["DATABASE_PORT"] ?? getenv('DATABASE_PORT');
 
 try {
-    $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+    $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
+
     $pdo = new PDO($dsn, $username, $password, [
-        PDO::MYSQL_ATTR_SSL_CA => "/etc/ssl/certs/ca-certificates.crt", // funciona no Railway e outros containers
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false, 
+        PDO::MYSQL_ATTR_SSL_CA => null 
     ]);
+    
+    echo "Conexão realizada com sucesso!";
 } catch (PDOException $e) {
     die("Erro de conexão: " . $e->getMessage());
 }
-?>
