c00ac2045b39d46cd25beaad73d0596c527ad3b1
GiselleBalieiro <gisellefbalieiro@outlook.com>
2025-09-14 17:00:55 -0300

    fix: ajusta cors agent.php

--- agent.php
+++ agent.php
@@ -12,9 +12,16 @@ $allowed_origins = [
 
 if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins)) {
     header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+    header("Access-Control-Allow-Credentials: true"); 
 }
 
 header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 require_once "db.php";
 
 $user_id = $_SESSION['user_id'] ?? null;
