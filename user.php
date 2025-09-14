04a1d09306ad0fc21e50a8ffa2257dcbade6ee6a
GiselleBalieiro <gisellefbalieiro@outlook.com>
2025-09-14 16:36:51 -0300

    fix: ajusta CORS

--- user.php
+++ user.php
@@ -1,5 +1,4 @@
 <?php
-
 header("Content-Type: application/json");
 
 $allowed_origins = [
@@ -10,9 +9,17 @@ $allowed_origins = [
 
 if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed_origins)) {
     header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+    header("Access-Control-Allow-Credentials: true"); 
 }
 
 header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 require_once "db.php";
 
 
