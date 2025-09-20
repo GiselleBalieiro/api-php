7ca9bf905e44b4f240dc48405be6a4859fe3b130
GiselleBalieiro <gisellefbalieiro@outlook.com>
2025-09-19 22:01:39 -0300

    fix: ajusta cors

--- agent.php
+++ agent.php
@@ -23,7 +23,7 @@ header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
+    echo json_encode(["status" => "ok"]);
     exit();
 }
 
